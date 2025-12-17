d09f6a0391a3b82ea3a34e2f513eb8fdf6e29ed9
Mikael Lirbank <mikael@lirbank.com>
2025-12-17 11:23:36 -0800

    Clear

--- app/api/ably-auth/route.ts
+++
@@ -1,8 +0,0 @@
-import { createTokenRequest } from "@/lib/ably-lib";
-import { NextResponse } from "next/server";
-
-export async function POST() {
-  const tokenRequest = await createTokenRequest();
-
-  return NextResponse.json(tokenRequest);
-}
