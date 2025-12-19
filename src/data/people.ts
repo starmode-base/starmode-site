e99e782ddb8562dede17071d71a5ec79ae091c90
Mikael Lirbank <mikael@lirbank.com>
2025-12-18 17:03:09 -0800

    People section

---
+++ src/data/people.ts
@@ -0,0 +1,18 @@
+export interface Person {
+  name: string;
+  role: string;
+  website?: string;
+}
+
+export const people: Person[] = [
+  {
+    name: "Mikael Lirbank",
+    role: "Founder, maintainer",
+    website: "https://www.lirbank.com/",
+  },
+  {
+    name: "Spencer Smith",
+    role: "Contributor",
+    website: "https://www.linkedin.com/in/spencergsmith6/",
+  },
+];
