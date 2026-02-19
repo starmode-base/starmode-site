280f974d1310653214a507542e8f2ca72d35047a
Mikael Lirbank <mikael@lirbank.com>
2026-02-19 08:35:21 -0800

    Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,12 @@
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 import icon from "astro-icon";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [icon()],
+  site: "https://www.starmode.dev",
+  integrations: [icon(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
